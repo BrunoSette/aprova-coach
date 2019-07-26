23a8b362f16030db00808256bbc0f6153a1f0011
BrunoSette <brunosette@gmail.com>
2019-07-26 12:16:16 -0300

    Update Jest Tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,71 @@ import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 import Header from './components/header/header';
+import Banner from './components/banner/banner';
+import Fazemos from './components/fazemos/fazemos';
+import Metodologia from './components/metodologia/metodologia';
+import Agende from './components/agende/agende';
+import Apresentacao from './components/apresentacao/apresentacao';
+import TestimonialsVideos from './components/testimonialsVideos/testimonialsVideos';
+import Testimonials from './components/testimonials/testimonials';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import ButtonAgendeSessao from './components/buttonAgendeSessao/buttonAgendeSessao';
+import Agendamento from './pages/agendamento';
+
+configure({ adapter: new Adapter() });
 
 //First Test
-configure({ adapter: new Adapter() });
-describe('First React component test with Enzyme', () => {
-  it('renders without crashing', () => {
-    shallow(<App />);
-  });
+it('renders without crashing', () => {
+  shallow(<App />);
 });
 
 //Second Test
-it('header snapshot should render correctly', () => {
+it('header snapshot renderered correctly', () => {
   const sidebar = shallow(<Header />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('banner snapshot renderered correctly', () => {
+  const sidebar = shallow(<Banner />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('fazemos snapshot renderered correctly', () => {
+  const sidebar = shallow(<Fazemos />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('metodologia snapshot renderered correctly', () => {
+  const sidebar = shallow(<Metodologia />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('agende snapshot renderered correctly', () => {
+  const sidebar = shallow(<Agende />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('apresentacao snapshot renderered correctly', () => {
+  const sidebar = shallow(<Apresentacao />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('testimonial videos snapshot renderered correctly', () => {
+  const sidebar = shallow(<TestimonialsVideos />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('testimonials snapshot renderered correctly', () => {
+  const sidebar = shallow(<Testimonials />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('contact snapshot renderered correctly', () => {
+  const sidebar = shallow(<Contact />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('footer snapshot renderered correctly', () => {
+  const sidebar = shallow(<Footer />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('buttonAgendeSessao snapshot renderered correctly', () => {
+  const sidebar = shallow(<ButtonAgendeSessao />);
+  expect(sidebar).toMatchSnapshot();
+});
+it('agendamento snapshot renderered correctly', () => {
+  const sidebar = shallow(<Agendamento />);
   expect(sidebar).toMatchSnapshot();
 });
