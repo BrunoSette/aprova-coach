c12394f5e3062714f629e7542a79b629ed1d304e
BrunoSette <brunosette@gmail.com>
2019-08-28 11:23:56 -0300

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,30 @@
 module.exports = {
-    
   siteMetadata: {
     title: `Aprova Coach`,
     siteUrl: `https://www.aprovacoach.com.br`,
     description: `Sua Coach Para Concursos`
   },
   plugins: [
-  {
-    resolve: 'gatsby-plugin-mixpanel',
-    options: {
-      apiToken: 'ed2e51f63610f4376ac1cb3aa056995d',
-      pageViews: {
-        '/': 'Home Page View', // an event 'Page blog view' will be send to mixpanel on every visit on the /blog page
-        '/agendamento': 'Agendamento View',
-        '/obrigado': 'Obrigado View',
-      },
-      mixpanelConfig: null, // you can override default config for mixpanel library https://github.com/mixpanel/mixpanel-js/blob/8b2e1f7b/src/mixpanel-core.js#L87-L110
-      /*
+    {
+      resolve: "gatsby-plugin-mixpanel",
+      options: {
+        apiToken: "ed2e51f63610f4376ac1cb3aa056995d",
+        pageViews: {
+          "/": "Home Page View", // an event 'Page blog view' will be send to mixpanel on every visit on the /blog page
+          "/agendamento": "Agendamento View",
+          "/obrigado": "Obrigado View"
+        },
+        mixpanelConfig: null // you can override default config for mixpanel library https://github.com/mixpanel/mixpanel-js/blob/8b2e1f7b/src/mixpanel-core.js#L87-L110
+        /*
       pageViews: 'all' // to track every route changes
       */
+      }
     },
-  },
-];
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-144564616-1"
+      }
+    }
+  ]
 };
