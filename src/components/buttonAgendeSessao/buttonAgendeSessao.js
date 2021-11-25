e93a6ce3e40365459431fbab1162f9ba61237319
Bruno Sette <brunosette@gmail.com>
2021-11-24 19:52:56 -0500

    Update butão Agende Gratis

--- src/components/buttonAgendeSessao/buttonAgendeSessao.js
+++ src/components/buttonAgendeSessao/buttonAgendeSessao.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function ButtonAgendeSessao() {
   return (
     <div className="text-align: center">
-      <Link to="/agendamento" target="_blank">
+      <Link to="https://wa.me/5581988668342?text=Ol%C3%A1%20Diana!%20Gostaria%20de%20saber%20mais%20sobre%Mentoria!" target="_blank">
         <Button variant="danger" size="lg">
           Agende Sessão Grátis!
         </Button>
